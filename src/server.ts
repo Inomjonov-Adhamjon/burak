dca01715b46deae132cc5968f4d8cc2e2e298791
Inomjonov-Adhamjon <adhamjon1713@gmail.com>
2026-09-24 08:40:04 +0900

    feat conntect via mongoose

--- src/server.ts
+++ src/server.ts
@@ -10,4 +10,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import mongoose from 'mongoose';
+mongoose.connect(process.env.MONGO_URL as string, {})
+    .then(data => {
+        console.log('mongodb connection succeed');
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch(err => console.log('error on connection on mongodb:', err));
 
