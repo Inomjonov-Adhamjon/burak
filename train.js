89ab84816d12e1a53de0f3385ac34569410a2639
Inomjonov-Adhamjon <adhamjon1713@gmail.com>
2026-09-24 09:30:28 +0900

    completed task M

--- train.js
+++ train.js
@@ -1,3 +1,26 @@
+// TASK M: 
+
+// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin
+// va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni 
+// kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+
+// yechim:
+function getSquareNumbers(numbers) {
+    const result = numbers.map((number) => {
+        return {
+            number: number,
+            square: number * number
+        }
+
+    });
+    return result
+}
+
+const result = getSquareNumbers([1, 2, 3, 4]);
+console.log(result)
+
+
+
 // TASK L
 // Shunday function yozing, u string qabul qilsin va string ichidagi 
 // hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan 
@@ -5,11 +28,11 @@
 
 
 // yechim:
-function reverseSentence(str) {
-    const word = str.split(" ")
-    const teskari = word.map((word) => word.split("").reverse().join(""))
+// function reverseSentence(str) {
+//     const word = str.split(" ")
+//     const teskari = word.map((word) => word.split("").reverse().join(""))
 
-    return teskari.join(" ")
-};
+//     return teskari.join(" ")
+// };
 
-console.log(reverseSentence("we like coding"));
+// console.log(reverseSentence("we like coding"));
