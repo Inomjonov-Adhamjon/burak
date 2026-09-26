f0b7577846fcf67d229ff7dc4eea4630e25b69c5
Inomjonov-Adhamjon <adhamjon1713@gmail.com>
2026-09-26 12:15:23 +0900

    feat: develop member schema model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,11 @@
+export enum MemberType {
+    USER = 'USER',
+    RESTAURANT = 'RESTAURANT',
+}
+
+export enum MemberStatus {
+    ACTIVE = 'ACTIVE',
+    RESTAURANT = 'RESTAURANT',
+    BLOCKED = 'BLOCKED',
+    DELETE = 'DELETE',
+}
---
+++ src/schema/Member.model.ts
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from 'mongoose';
+import { MemberStatus, MemberType } from '../libs/enums/member.enum';
+// schema first and code based
+
+const memberSchema = new Schema({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        default: MemberType.USER
+    },
+    memberStatus: {
+        type: String,
+        enum: MemberStatus,
+        default: MemberStatus.ACTIVE
+    },
+    memberNick: {
+        type: String,
+        index: { unique: true, sparse: true },
+        required: true
+    },
+    memberPhone: {
+        type: String,
+        index: { unique: true, sparse: true },
+        required: true
+    },
+
+    memberPassword: {
+        type: String,
+        select: false,
+        required: true,
+    },
+
+    memberAddress: {
+        type: String,
+    },
+    memberDescription: {
+        type: String,
+    },
+
+    memberImage: {
+        type: String,
+    },
+
+    memberPoints: {
+        type: Number,
+        default: 0,
+    },
+},
+    { timestamps: true }
+);
+
+export default mongoose.model('member', memberSchema)
