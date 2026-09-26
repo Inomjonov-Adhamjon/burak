fe8a357d81565fd3bda1c8ef78be462c833c43c7
Inomjonov-Adhamjon <adhamjon1713@gmail.com>
2026-09-26 11:55:40 +0900

    feat: member service model integration

--- src/controller/restaurant.controller.ts
+++ src/controller/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import { T } from '../libs/types/common';
 import { Request, Response } from 'express';
+import MemberService from '../modules/Members.service';
 
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
