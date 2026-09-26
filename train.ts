dfd2e0de50ae0cb56557ce3df7ce570bb3a2113c
Inomjonov-Adhamjon <adhamjon1713@gmail.com>
2026-09-26 12:30:44 +0900

    complted task N

---
+++ train.ts
@@ -0,0 +1,52 @@
+// Task N 
+// Shunday function yozing, u string qabul qilsin va string palindrom yani togri 
+// oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+
+// yechimi:
+function polindromCheck(str: string): boolean {
+    const reversed = str.split("").reverse().join("")
+    return str === reversed
+}
+const result = polindromCheck("dad");
+console.log("result:", result)
+
+
+
+// TASK M: 
+
+// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin
+// va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni 
+// kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+
+// yechim:
+// function getSquareNumbers(numbers) {
+//     const result = numbers.map((number) => {
+//         return {
+//             number: number,
+//             square: number * number
+//         }
+
+//     });
+//     return result
+// }
+
+// const result = getSquareNumbers([1, 2, 3, 4]);
+// console.log(result)
+
+
+
+// TASK L
+// Shunday function yozing, u string qabul qilsin va string ichidagi 
+// hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan 
+// stringni qaytarsin.
+
+
+// yechim:
+// function reverseSentence(str) {
+//     const word = str.split(" ")
+//     const teskari = word.map((word) => word.split("").reverse().join(""))
+
+//     return teskari.join(" ")
+// };
+
+// console.log(reverseSentence("we like coding"));
